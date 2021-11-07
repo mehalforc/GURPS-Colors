6c6967b724396a59594553515b1a14c0f3fec9cb
mehalforc <sdberndt@charter.net>
2021-11-07 16:36:45 -0600

    Add files via upload

--- scripts/jquery.tinycolorpicker.js
+++ scripts/jquery.tinycolorpicker.js
@@ -1,5 +1,3 @@
-// https://github.com/wieringen/tinycolorpicker
-// Changed line 120 to work with Foundry VTT and GURPS-Colors Module
 ;(function(factory) {
     if(typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
